37f0d72446d69f3e45ec873347f178048797ce08
s-xq <971070045@qq.com>
2018-05-01 21:51:58 +0800

    spring boot init commit

---
+++ src/main/java/com/sxq/eurekaclientservicehello/EurekaClientServiceHelloApplication.java
@@ -0,0 +1,12 @@
+package com.sxq.eurekaclientservicehello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientServiceHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientServiceHelloApplication.class, args);
+	}
+}
