2d049118836c72ecdc8372fefc32d9a62d364171
s-xq <971070045@qq.com>
2018-05-01 23:35:15 +0800

    配置eureka client，遇到的坑：client未能自动发现server，待解决

--- src/main/java/com/sxq/eurekaclientservicehello/EurekaClientServiceHelloApplication.java
+++ src/main/java/com/sxq/eurekaclientservicehello/EurekaClientServiceHelloApplication.java
@@ -2,7 +2,9 @@ package com.sxq.eurekaclientservicehello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaClientServiceHelloApplication {
 
